4faf6314df7b96f1dfa17dd05b34f653c381aeb8
neo938 <neo938@gmail.com>
2020-04-15 20:30:39 +0200

    Commit

---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour
+{
+    public Text speedValue;
+    public CarMovementScript cms;
+    public GameObject sun;
+    private void OnGUI()
+    {
+        speedValue.text = cms.GetSpeed(0).ToString();
+    }
+
+    public void SwitchDayNight()
+    {
+        sun.SetActive(!sun.activeInHierarchy);
+    }
+    
+
+}
