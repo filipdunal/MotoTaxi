76fe772397d1ce407786f15e4e089cc66e2a16f9
neo938 <neo938@gmail.com>
2020-06-22 00:46:50 +0200

    Dodano model wirtualnego telefonu, jego animację i wygaszanie UI

--- Assets/FitInCamera.cs
+++ Assets/FitInCamera.cs
@@ -1,54 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class FitInCamera : MonoBehaviour
 {
     public Camera cameraToFit;
     public bool openedPhone;
+    public GraphicRaycaster graphicRaycaster;
+    public float phoneAnimationSpeed=1f;
+    public GameObject hidePhoneButton;
+    public List<Transform> steeringButtons;
 
     float ratio16v9 = 16f / 9f;
-    private void Update()
-    {
-        
-    }
-
+    
     public void OpenPhone()
     {
-        transform.localPosition = new Vector3(0f,0f,transform.localPosition.z);
-        transform.rotation = Quaternion.identity;
+        Vector3 position = new Vector3(0f, 0f, transform.localPosition.z);
+        Quaternion rotation = Quaternion.identity;
+        Vector3 scale;
+
+        graphicRaycaster.enabled = true;
+
+        //transform.localPosition = new Vector3(0f,0f,transform.localPosition.z);
+        //transform.rotation = Quaternion.identity;
 
         float height = 2.0f * Mathf.Tan(0.5f * cameraToFit.fieldOfView * Mathf.Deg2Rad) * transform.localPosition.z;
         float width = height * cameraToFit.aspect;
         if (cameraToFit.aspect > ratio16v9)
         {
             height *= 1.4f;
-            transform.localScale = new Vector3(height, height, transform.localScale.z);
+            scale = new Vector3(height, height, transform.localScale.z);
         }
         else
         {
             width *= 0.8f;
-            transform.localScale = new Vector3(width, width, transform.localScale.z);
+            scale = new Vector3(width, width, transform.localScale.z);
         }
+        StopAllCoroutines();
+        StartCoroutine(PhoneAnimation(position, rotation, scale));
+        hidePhoneButton.SetActive(true);
+
+        foreach(Transform button in steeringButtons)
+        {
+            button.gameObject.SetActive(false);
+        }
+        
     }
 
     public void ClosePhone()
     {
-        transform.localPosition = new Vector3(0f, -0.7f, transform.localPosition.z);
-        transform.localEulerAngles = new Vector3(70f, 0f, 0f);
-        transform.localScale = new Vector3(1f, 1f, transform.localScale.z);
+        hidePhoneButton.SetActive(false);
+        graphicRaycaster.enabled = false;
+
+        Vector3 position = new Vector3(0f, -0.7f, transform.localPosition.z);
+        Quaternion rotation = Quaternion.Euler(70f, 0f, 0f);
+        Vector3 scale = new Vector3(1f, 1f, transform.localScale.z);
+        StopAllCoroutines();
+        StartCoroutine(PhoneAnimation(position, rotation, scale));
+
+        foreach (Transform button in steeringButtons)
+        {
+            button.gameObject.SetActive(true);
+        }
     }
 
-    private void OnMouseDown()
+    IEnumerator PhoneAnimation(Vector3 position, Quaternion rotation, Vector3 scale)
     {
-        openedPhone = !openedPhone;
-        if(openedPhone)
+        
+        while (Vector3.Distance(transform.localScale, scale)>0.001f)
         {
-            OpenPhone();
+            transform.localPosition = Vector3.Lerp(transform.localPosition, position, phoneAnimationSpeed * Time.unscaledDeltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, phoneAnimationSpeed * Time.unscaledDeltaTime);
+            transform.localScale = Vector3.Lerp(transform.localScale, scale, phoneAnimationSpeed * Time.unscaledDeltaTime);
+            yield return 0;
         }
-        else
+        
+    }
+
+    private void OnMouseUp()
+    {
+        if(!IsPointerOverUIObject())
         {
-            ClosePhone();
+            openedPhone = !openedPhone;
+            if (openedPhone)
+            {
+                OpenPhone();
+            }
         }
+        
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
